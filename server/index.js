05471db39fc03c734584fbb9a12c35c0a1617464
Salman Hamza <90480347+salmanhamza@users.noreply.github.com>
2022-08-26 03:29:08 -0700

    commit

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ import express from "express";
 
 import dotenv from "dotenv";
 import Connection from "./database/db.js";
+import postRoutes from "./Routes/route.js";
+import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
 dotenv.config();
+
+app.use("/posts", postRoutes);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
